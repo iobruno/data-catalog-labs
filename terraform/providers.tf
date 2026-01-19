0ec0f782a2af3bb917d11763898a4213ef58a595
iobruno <bruno.ariev@gmail.com>
2026-01-19 15:05:03 -0300

    feat/iac-airbyte-gcp: IaC for GCP BigQuery & Airbyte OSS
    * Configure GCS as backend to save state
    
    airbyte/
    * Add Hacker RSS feeds as Data Sourcee for Airbyte
    * Add BigQuery as Destination Airbyte
    * Wires the connection between Sources and BigQuery destination
    
    gcp_bigquery/
    * Add BigQuery dataset for hackernews_rss_feed raw ingestion
    * Add .tfvars.example as template to set up the required vars
    * Update running instructions on README.md accordingly

---
+++ terraform/providers.tf
@@ -0,0 +1,29 @@
+terraform {
+    required_providers {
+        airbyte = {
+            source = "airbytehq/airbyte"
+            version = "0.6.5"
+        }
+        google = {
+            source  = "hashicorp/google"
+            version = "7.17.0"
+        }
+    }
+
+    backend "gcs" {
+        bucket = "iobruno-gcp-labs-tfstate"
+        prefix = "data-catalog-labs"
+    }
+}
+
+provider "airbyte" {
+    client_id     = var.airbyte_client_id
+    client_secret = var.airbyte_client_secret
+
+    server_url = "http://localhost:8000/api/public/v1/"
+}
+
+provider "google" {
+    project = var.gcp_project_id
+    region  = var.gcp_data_region
+}
