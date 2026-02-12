da5a827fd3330df1371938ee2fe8209c9a4baf26
Bruno Oliveira <bruno.ariev@gmail.com>
2026-02-12 12:14:43 -0300

    feat/datahub-airbyte-lineage: Airbyte Connections as data lineage
    * Add DockerOperator tasks to ingest Airbyte metadata into DataHub
      with upstream (Airflow) and downstream (BigQuery) lineage
    * Resolve Airbyte credentials (server_url, client_id, client_secret)
      from connection `airbyte_default`

--- airflow/dags/hackernews_rss_bigquery.py
+++ airflow/dags/hackernews_rss_bigquery.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 
 from airflow import DAG
+from airflow.hooks.base import BaseHook
 from airflow.providers.airbyte.operators.airbyte import AirbyteTriggerSyncOperator
 from airflow.providers.docker.operators.docker import DockerOperator
 from docker.types import Mount
@@ -8,6 +9,7 @@ from docker.types import Mount
 HACKERNEWS_RSS_FRONT_CONN_ID = "e37988e6-8ed5-465c-abb2-150639819c62"
 HACKERNEWS_RSS_COMMENTS_CONN_ID = "2fda0c2f-6a50-427e-af1b-25050ad40384"
 HACKERNEWS_RSS_NEWEST_CONN_ID = "4ca6f367-93f7-4f88-b0fc-b765daf09a28"
+airbyte_conn = BaseHook.get_connection("airbyte_default")
 
 with DAG(
     dag_id="hackernews_rss_bigquery",
@@ -72,7 +74,57 @@ with DAG(
         ],
     )
 
+    airbyte_lineage_hackernews_rss_front = DockerOperator(
+        task_id="airbyte_lineage_hackernews_rss_front",
+        image="datahub-ingest-airbyte:latest",
+        auto_remove="force",
+        network_mode="bridge",
+        environment={
+            "AIRBYTE_CONNECTION_ID": HACKERNEWS_RSS_FRONT_CONN_ID,
+            "AIRBYTE_SERVER_URL": airbyte_conn.host,
+            "AIRBYTE_CLIENT_ID": airbyte_conn.login,
+            "AIRBYTE_CLIENT_SECRET": airbyte_conn.password,
+            "AIRFLOW_DAG_NAME": dag.dag_id,
+            "AIRFLOW_TASK_NAME": hackernews_rss_front.task_id,
+        },
+    )
+
+    airbyte_conn_hackernews_rss_newest = DockerOperator(
+        task_id="airbyte_lineage_hackernews_rss_newest",
+        image="datahub-ingest-airbyte:latest",
+        auto_remove="force",
+        network_mode="bridge",
+        environment={
+            "AIRBYTE_CONNECTION_ID": HACKERNEWS_RSS_NEWEST_CONN_ID,
+            "AIRBYTE_SERVER_URL": airbyte_conn.host,
+            "AIRBYTE_CLIENT_ID": airbyte_conn.login,
+            "AIRBYTE_CLIENT_SECRET": airbyte_conn.password,
+            "AIRFLOW_DAG_NAME": dag.dag_id,
+            "AIRFLOW_TASK_NAME": hackernews_rss_newest.task_id,
+        },
+    )
+
+    airbyte_conn_hackernews_rss_comments = DockerOperator(
+        task_id="airbyte_conn_hackernews_rss_comments",
+        image="datahub-ingest-airbyte:latest",
+        auto_remove="force",
+        network_mode="bridge",
+        environment={
+            "AIRBYTE_CONNECTION_ID": HACKERNEWS_RSS_COMMENTS_CONN_ID,
+            "AIRBYTE_SERVER_URL": airbyte_conn.host,
+            "AIRBYTE_CLIENT_ID": airbyte_conn.login,
+            "AIRBYTE_CLIENT_SECRET": airbyte_conn.password,
+            "AIRFLOW_DAG_NAME": dag.dag_id,
+            "AIRFLOW_TASK_NAME": hackernews_rss_comments.task_id,
+        },
+    )
+
     (
         [hackernews_rss_front, hackernews_rss_newest, hackernews_rss_comments]
         >> dbt_execution
+        >> [
+            airbyte_lineage_hackernews_rss_front,
+            airbyte_conn_hackernews_rss_newest,
+            airbyte_conn_hackernews_rss_comments,
+        ]
     )
