decd7704e566ae453f371253b4acf38412591f60
Bruno Oliveira <bruno.ariev@gmail.com>
2026-07-11 19:07:48 -0300

    Revert "fix/duplicate uid in dim_articles & add source_feed col (#3)"
    This reverts commit 4b4a80ad90987a6e89ab2e848703dd7b9b0d778b.

--- dbt/models/core/dim_articles.sql
+++ dbt/models/core/dim_articles.sql
@@ -2,39 +2,10 @@
     schema=resolve_schema_for('core')
 ) }}
 
-with combined_articles as (
-    select
-        h.uid,
-        h.title,
-        h.username,
-        h.url,
-        h.redirect_url,
-        h.published_at,
-        'hot' as source_feed
-    from 
-        {{ ref('stg_hot_articles') }} h
+select h.* 
+from {{ ref('stg_hot_articles') }} h
 
-    union all
+union distinct
 
-    select
-        n.uid,
-        n.title,
-        n.username,
-        n.url,
-        n.redirect_url,
-        n.published_at,
-        'newest' as source_feed
-    from 
-        {{ ref('stg_newest_articles') }} n
-)
-
-select * from combined_articles
-qualify row_number() over (
-    partition by 
-        uid 
-    order by
-        case source_feed
-            when 'hot' then 1
-            else 2
-        end
-) = 1
+select n.* 
+from {{ ref('stg_newest_articles') }} n
