4a6e2e498c51799a8759f139995ed932bbaf54b8
Bruno Oliveira <bruno.ariev@gmail.com>
2026-07-11 19:12:50 -0300

    Fix duplicate uid in dim_articles, add source_feed column (#6)
    * UNION DISTINCT missed dupes differing only in case
       e.g. "JavaScript Runtime" vs "...runtime", failing the unique_test on `uid`
    * Dedupe on `uid` via `QUALIFY ROW_NUMBER()`
    * Add `source_feed` (`hot`/`newest`) column to track row origin

--- dbt/models/core/dim_articles.sql
+++ dbt/models/core/dim_articles.sql
@@ -2,10 +2,39 @@
     schema=resolve_schema_for('core')
 ) }}
 
-select h.* 
-from {{ ref('stg_hot_articles') }} h
+with combined_articles as (
+    select
+        h.uid,
+        h.title,
+        h.username,
+        h.url,
+        h.redirect_url,
+        h.published_at,
+        'hot' as source_feed
+    from 
+        {{ ref('stg_hot_articles') }} h
 
-union distinct
+    union all
 
-select n.* 
-from {{ ref('stg_newest_articles') }} n
+    select
+        n.uid,
+        n.title,
+        n.username,
+        n.url,
+        n.redirect_url,
+        n.published_at,
+        'newest' as source_feed
+    from 
+        {{ ref('stg_newest_articles') }} n
+)
+
+select * from combined_articles
+qualify row_number() over (
+    partition by 
+        uid 
+    order by
+        case source_feed
+            when 'hot' then 1
+            else 2
+        end
+) = 1
